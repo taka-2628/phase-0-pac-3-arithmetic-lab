ebf218541a5a3eb2b6788a3a4125cc2253382f7b
Takashi <th2826@columbia.edu>
2021-07-29 16:19:32 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+/*
+//basic math functions
+console.log(subtract(6, 3));
+console.log(divide(12, 8));
+
+//increment&decrement
+console.log(increment(1));
+console.log(decrement(0));
+
+//parseInt() & parseFloat()
+console.log(makeInt(5.55));
+console.log(preserveDecimal(5.55));
+*/
